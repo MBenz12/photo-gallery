94d8828ebb0d4be3fe26628f1d22aad0d635dc3a
mbenz12 <mbenz10120112@gmail.com>
2024-06-21 08:48:09 -0400

    vite config updated for bip39

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,64 @@
-import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import inject from '@rollup/plugin-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy()
+    tsconfigPaths(),
+    inject({
+      // Polyfill Buffer in the browser
+      Buffer: ['buffer', 'Buffer'],
+    }),
   ],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+  },
+  server: {
+    host: true,
+    port: 8100,
+    watch: {
+      usePolling: true,
+    }
+  },
+  optimizeDeps: {
+    include: ['bip39'],
+    exclude: ['bip39/wordlists'],
+    esbuildOptions: {
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        }),
+        NodeModulesPolyfillPlugin()
+      ]
+    }
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        inject({
+          Buffer: ['buffer', 'Buffer'],
+        })
+      ],
+      external: [
+        /^bip39\/wordlists\/(?!english).*\.json$/
+      ],
+      output: {
+        manualChunks(id) {
+          if (/node_modules/.test(id)) {
+            return 'vendor';
+          }
+          if (/.*\/wordlists\/(?!english).*\.json/.test(id)) {
+            return 'bip39-wordlists';
+          }
+        }
+      }
+    }
   }
 })
